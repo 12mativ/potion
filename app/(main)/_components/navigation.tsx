016ec6cc4f9aad8a9306d9ffc2faf60743006527
12mativ <ivan_m146@mail.ru>
2024-09-04 23:46:34 +0400

    feature: add toolbar component

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from "lucide-react";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { cn } from "../../../lib/utils";
@@ -15,11 +15,13 @@ import { Popover, PopoverContent, PopoverTrigger } from "../../../components/ui/
 import { TrashBox } from "./trash-box";
 import { useSearch } from "../../../hooks/use-search";
 import { useSettings } from "../../../hooks/use-settings";
+import { Navbar } from "./navbar";
 
 export const Navigation = () => {
   const search = useSearch();
   const settings = useSettings();
   const pathname = usePathname()
+  const params = useParams();
   const isMobile = useMediaQuery("(max-width: 768px)")
   const create = useMutation(api.documents.create) 
 
@@ -186,9 +188,17 @@ export const Navigation = () => {
           isMobile && "left-0 w-full"
         )}
       >
-        <nav className="bg-transparent px-3 py-2 w-full">
-          {isCollapsed && <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground" />}
-        </nav>
+        {!!params.documentId ? (
+          <Navbar 
+            isCollapsed={isCollapsed}
+            onResetWidth={resetWidth}
+          />
+        ) : (
+          <nav className="bg-transparent px-3 py-2 w-full">
+            {isCollapsed && <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground" />}
+          </nav>
+        )}
+        
       </div>
     </>
     
